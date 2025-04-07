d903158f9c6850378cb440cab8010fbada3bb980
Andi Fathul Mukminin <officialandifathul@gmail.com>
2025-04-07 14:56:41 +0700

    fix lint

--- familylineage/persons/forms.py
+++ familylineage/persons/forms.py
@@ -7,12 +7,12 @@ from familylineage.apps.relationships.models import Marriage, ParentChild
 class AddParentForm(forms.Form):
     parents = forms.ModelChoiceField(queryset=None)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.fields['parents'].queryset = Marriage.objects.all()
         self.fields['parents'].label_from_instance = lambda obj: f"{obj.husband.name} & {obj.wife.name}"
 
-    def save(self, person: Person):
+    def save(self, person: Person) -> None:
         parents = self.cleaned_data['parents']
         husband = parents.husband
         wife = parents.wife
