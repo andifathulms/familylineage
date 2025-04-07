c342fee0f61566bc0a1ceaebccbbd3aedef4d6b5
Andi Fathul Mukminin <officialandifathul@gmail.com>
2025-04-07 14:55:12 +0700

    add parents

---
+++ familylineage/persons/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from familylineage.apps.persons.models import Person
+from familylineage.apps.relationships.models import Marriage, ParentChild
+
+
+class AddParentForm(forms.Form):
+    parents = forms.ModelChoiceField(queryset=None)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['parents'].queryset = Marriage.objects.all()
+        self.fields['parents'].label_from_instance = lambda obj: f"{obj.husband.name} & {obj.wife.name}"
+
+    def save(self, person: Person):
+        parents = self.cleaned_data['parents']
+        husband = parents.husband
+        wife = parents.wife
+
+        ParentChild.objects.create(parent=husband, child=person)
+        ParentChild.objects.create(parent=wife, child=person)
--- familylineage/persons/views.py
+++ familylineage/persons/views.py
@@ -1,8 +1,11 @@
+from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from familylineage.apps.persons.models import Person
+
+from .forms import AddParentForm
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -24,3 +27,15 @@ def index(request: HttpRequest) -> HttpResponse:
 def details(request: HttpRequest, person_id: int) -> HttpResponse:
     person = Person.objects.get(pk=person_id)
     return render(request, 'persons/details.html', {'person': person})
+
+
+def add_parents(request: HttpRequest, person_id: int) -> HttpResponse:
+    person = Person.objects.get(pk=person_id)
+
+    form = AddParentForm(request.POST or None)
+    if form.is_valid():
+        form.save(person=person)
+        messages.success(request, 'Parents added successfully')
+        return redirect("persons:details", person_id=person.id)
+
+    return render(request, 'forms.html', {'person': person, 'form': form})
